ec813a79dbd4124737f47eb12be3f86435e218c2
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-03-23 16:39:54 +0500

    add

--- ArduinoCarControl/Form1.Designer.cs
+++ ArduinoCarControl/Form1.Designer.cs
@@ -172,7 +172,7 @@
             button2.ForeColor = Color.Transparent;
             button2.Location = new Point(0, 115);
             button2.Name = "button2";
-            button2.Size = new Size(561, 53);
+            button2.Size = new Size(511, 53);
             button2.TabIndex = 1;
             button2.Text = "Turn the car on or off";
             button2.TextAlign = ContentAlignment.MiddleLeft;
